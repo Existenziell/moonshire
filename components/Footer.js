5f9cd0096cc735b8d63a47781ea5d092beeb3107
Christof <bauer.christof@gmail.com>
2022-06-06 17:53:22 -0500

    Margins!

--- components/Footer.js
+++ components/Footer.js
@@ -25,13 +25,10 @@ const Footer = () => {
   }, [inView, fadeIn])
 
   return (
-    <footer ref={ref} className='static bottom-0 flex items-center justify-between px-10 h-20 text-xs text-black bg-brand dark:text-white dark:bg-brand-dark'>
+    <footer ref={ref} className='static bottom-0 flex items-center justify-between px-[40px] h-20 text-xs text-black bg-brand dark:text-white dark:bg-brand-dark'>
       <div className='flex items-center justify-between w-full'>
         <DarkModeToggle />
         <div>
-
-
-
           <span className='text-tiny'>&copy; 2022 FE1</span>
           <Link href='/about'><a className={`${router.pathname === '/about' ? 'active-nav' : ''} link text-tiny ml-4 uppercase font-serif`}>About</a></Link>
           <Link href='/imprint'><a className={`${router.pathname === '/imprint' ? 'active-nav' : ''} link text-tiny ml-4 uppercase font-serif`}>Imprint</a></Link>
--- components/_Layout.js
+++ components/_Layout.js
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
         options={{ showSpinner: false }}
       />
 
-      <div className='pt-5 px-10 hidden md:flex justify-between items-start z-20 bg-brand dark:bg-brand-dark text-black dark:text-white' >
+      <div className='pt-[20px] px-[40px] hidden md:flex justify-between items-start z-20 bg-brand dark:bg-brand-dark text-black dark:text-white' >
         <Logo />
         <div className='flex items-center'>
           <Nav />
@@ -26,7 +26,7 @@ const Layout = ({ children }) => {
         </div>
       </div>
 
-      <div className='pt-5 px-10 flex md:hidden justify-between items-start z-20 bg-brand dark:bg-brand-dark text-black dark:text-white' >
+      <div className='pt-[20px] px-[40px] flex md:hidden justify-between items-start z-20 bg-brand dark:bg-brand-dark text-black dark:text-white' >
         <div>
           <Logo />
         </div>
@@ -36,7 +36,7 @@ const Layout = ({ children }) => {
         </div>
       </div>
 
-      <main className='w-full text-black bg-brand dark:text-white dark:bg-brand-dark min-h-[calc(100vh-160px)] px-10 py-[60px]'>
+      <main className='w-full text-black bg-brand dark:text-white dark:bg-brand-dark min-h-[calc(100vh-160px)] px-[40px] py-[60px]'>
         {children}
       </main>
 
