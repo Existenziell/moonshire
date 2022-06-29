4f5dbfd7e06d465f379d46ff938a706e5c96fbb4
Christof <bauer.christof@gmail.com>
2022-06-29 17:48:44 -0500

    Change font in Search to Arial

--- components/admin/Search.js
+++ components/admin/Search.js
@@ -3,7 +3,7 @@ import { XIcon } from "@heroicons/react/solid"
 const Search = ({ search, setSearch, resetSearch }) => {
   return (
     <div className='relative'>
-      <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} name='search' placeholder='Search' className='ml-2 border-detail focus:ring-cta focus:border-none dark:border-detail-dark' />
+      <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} name='search' placeholder='Search' className='ml-2 border-detail focus:ring-cta focus:border-none dark:border-detail-dark font-serif' />
       <button onClick={resetSearch} className='absolute top-3 right-2 text-brand-dark/20 dark:text-brand/20 hover:text-cta dark:hover:text-cta h-max'>
         <XIcon className='w-5' />
       </button>
