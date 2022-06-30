51eade88572d8f09dddc0308c8987bf4580f63dd
Christof <bauer.christof@gmail.com>
2022-06-30 14:04:08 -0500

    Artists: Disable link to detail-page and show asset block

--- pages/artists/index.js
+++ pages/artists/index.js
@@ -15,7 +15,7 @@ const Artists = ({ artists }) => {
         <div className='md:snap-y md:snap-mandatory md:h-[calc(100vh-200px)] md:overflow-y-scroll'>
 
           {artists.map(artist => {
-            const { id, name, headline, description, origin, public_url } = artist
+            const { id, name, headline, description, public_url, collections, numberOfCollections, nfts, numberOfNfts } = artist
             return (
               <div key={id} className='md:snap-start md:snap-always md:h-[calc(100vh-200px)] w-full mb-40'>
                 <div className={`flex flex-col md:flex-row items-center justify-center gap-[40px] px-[40px]`}>
@@ -27,14 +27,30 @@ const Artists = ({ artists }) => {
                     <hr className='mt-8 mb-12' />
                     <p>{headline}</p>
                     <p className='mt-4'>{description}</p>
-                    <p className='mt-4'>Origin: {origin}</p>
+                    {/* <p className='mt-4'>Origin: {origin}</p> */}
                     {/* <p>{numberOfNfts} artworks by <span className='text-white'>{name}</span></p> */}
                     {/* <p className='mt-8 text-xs'>On Moonshire since: {created_at.slice(0, 10)}</p> */}
-                    <Link href={`/artists/${id}`}>
+                    {/* <Link href={`/artists/${id}`}>
                       <a className='button button-cta mt-8 mx-auto md:mr-auto md:ml-0 uppercase'>
                         Show Profile
                       </a>
-                    </Link>
+                    </Link> */}
+                    <div className='mt-16'>
+                      <h1 className='mb-0'>Assets</h1>
+                      <hr className='my-8' />
+                      <p className='mb-4'>{numberOfCollections} {numberOfCollections > 1 ? `Collections` : `Collection`}</p>
+                      <div>
+                        {collections.map(c => (
+                          <Link key={c.id} href={`/collections/${c.id}`}><a className='link-white block'>{c.title}</a></Link>
+                        ))}
+                      </div>
+                      <p className='mb-4 mt-8'>{numberOfNfts} {numberOfNfts > 1 ? `NFTs` : `NFT`}</p>
+                      <div>
+                        {nfts.map(n => (
+                          <Link key={n.id} href={`/nfts/${n.id}`}><a className='link-white block'>{n.name}</a></Link>
+                        ))}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -51,14 +67,27 @@ const Artists = ({ artists }) => {
 }
 
 export async function getServerSideProps() {
-  const { data: artists } = await supabase.from('artists').select(`*`).order('id', { ascending: true })
-  const { data: nfts } = await supabase.from('nfts').select(`*, artists(*)`).order('id', { ascending: true })
+  const { data: artists } = await supabase.from('artists').select(`*`).order('created_at', { ascending: false })
+  const { data: nfts } = await supabase.from('nfts').select(`*, artists(*), collections(*)`).order('created_at', { ascending: false })
 
   for (let artist of artists) {
     const url = await getPublicUrl('artists', artist.avatar_url)
     artist.public_url = url
-    const collectionNfts = nfts.filter((n => n.artist === artist.id))
-    artist.numberOfNfts = collectionNfts.length
+
+    let collections = []
+    for (let nft of nfts) {
+      if (nft.artist === artist.id) collections.push(nft.collections)
+    }
+    /* eslint-disable no-undef */
+    let uniqueCollections = [...new Map(collections.map((item) => [item['id'], item])).values()];
+    /* eslint-enable no-undef */
+
+    artist.collections = uniqueCollections
+    artist.numberOfCollections = uniqueCollections.length
+
+    const filteredNfts = nfts.filter((n => n.artist === artist.id))
+    artist.nfts = filteredNfts
+    artist.numberOfNfts = filteredNfts.length
   }
 
   return {
