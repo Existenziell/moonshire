b6f3f6492ccc3f37db1d987fc55211025c3c8a9d
Christof <bauer.christof@gmail.com>
2022-08-12 21:44:08 -0500

    Lint...

--- components/NftsGrid.js
+++ components/NftsGrid.js
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { shortenAddress } from '../lib/shortenAddress'
+// import { shortenAddress } from '../lib/shortenAddress'
 import fromExponential from 'from-exponential'
 
 const NftsGrid = ({ nfts }) => {
