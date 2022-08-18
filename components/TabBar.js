88bb4e3ac7377dfc42ab726361bdb2695b94db60
Christof <bauer.christof@gmail.com>
2022-08-18 14:12:14 -0500

    Hide number of results

--- components/TabBar.js
+++ components/TabBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import Link from "next/link"
 import Search from "./Search"
 
@@ -23,7 +24,7 @@ const TabBar = ({ links, extra, view, navigate, setDisplay, display, sortBy, sor
 
       {view !== 'settings' &&
         <div className='hidden md:flex items-center gap-8 relative bottom-2'>
-          <span className='text-brand-dark/20 dark:text-white'>{length} results</span>
+          {/* <span className='text-brand-dark/20 dark:text-white'>{length} results</span> */}
 
           <button onClick={() => setDisplay('grid')}>
             <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"
