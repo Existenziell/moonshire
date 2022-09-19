1ceb0f3c75428b4f8d09f2d906df6b9130169705
Christof <bauer.christof@gmail.com>
2022-09-19 13:45:54 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -9,9 +9,9 @@ const About = () => {
       </Head>
 
       <div className='flex flex-col items-center justify-center max-w-xl text-sm text-justify leading-relaxed mx-auto px-[20px] md:px-[40px]'>
-        {/* <h1>Web3 SaaS Provider</h1> */}
-        {/* <p className='mb-6'>Project Moonshire is a web3 white-label platform disrupting the 2.0 status quo: A cutting edge, decentralised monetising service for content creators running on the Rinkeby Blockchain..</p>
-        <p>With Project Moonshire creators are given full control of their releases offering a direct channel for their community of fans to collect and trade releases by their idols.</p> */}
+        <h1>Web3 SaaS Provider</h1>
+        <p className='mb-6'>Project Moonshire is a web3 white-label platform disrupting the 2.0 status quo: A cutting edge, decentralised monetising service for content creators running on the Blockchain.</p>
+        <p>With Project Moonshire creators are given full control of their releases offering a direct channel for their community of fans to collect and trade releases by their idols.</p>
       </div>
     </>
   )
